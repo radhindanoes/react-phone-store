2956fc0bcd58aac8b636aecf0ab3ee08801e6af7
radhindanoes <radhindanoes@gmail.com>
2019-05-27 07:34:40 -0500

    added hidden key again

--- src/components/Cart/PayPalButton.js
+++ src/components/Cart/PayPalButton.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import PaypalExpressBtn from 'react-paypal-express-checkout';
- 
+import evviroment from '.'
 export default class MyApp extends React.Component {
     render() {
         const onSuccess = (payment) => {
@@ -30,7 +30,7 @@ export default class MyApp extends React.Component {
         // Document on Paypal's currency code: https://developer.paypal.com/docs/classic/api/currency_codes/
  
         const client = {
-            sandbox:    'AWJcSgUq41m1DCqzvvgdIpTjXNQ0fnP80Bn7BfIGy3lrL7-99XLRkOikFPjBfKxbLR6FpsZFXjWz_ORZ',
+            sandbox:    process.env.REACT_APP_APP_ID,
             production: 'YOUR-PRODUCTION-APP-ID',
         }
         // In order to get production's app-ID, you will have to send your app to Paypal for approval first
